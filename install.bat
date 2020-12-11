89156fe012db907e71d595613b4f3c0fb11c0008
ellisonguimaraes <ellison.guimaraes@gmail.com>
2020-12-11 20:23:31 -0300

    update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,15 @@
+@ECHO OFF
+IF EXIST .\.venv\ GOTO NOVENV
+ECHO "*************** Criando um Ambiente Virtual ***************"
 python -m venv .venv
+ECHO "*************** Criando o Projeto Flask ***************"
 .venv\Scripts\pip.exe install -e .
+ECHO "*************** Executando o Projeto Flask ***************"
 run.bat
+pause
+
+
+:NOVENV
+ECHO "*************** Executando o Projeto Flask ***************"
+run.bat
+pause
